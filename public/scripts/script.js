58df40781c5c03a1c6f0d6526b538946498866ca
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-10 15:57:02 -0500

    trying to get post to work

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,15 +7,20 @@ myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
 
   vm.addRecord = function(){
     console.log("in add record route on client");
+
     var objectToSend ={
       name : vm.nameIn,
       location: vm.locationIn
     };
+
     $http({
       method: 'POST',
       url: '/testPost',
       data: objectToSend
+    }).then ( function(response){
+      console.log(response);
     });
+
     vm.nameIn ='';
     vm.locationIn='';
   };
@@ -23,11 +28,13 @@ myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
 
   vm.getRecords = function(){
     console.log("in getrecords route on client");
+
     $http({
       method: 'GET',
       url: '/getRecords',
     }).then( function( response ){
-      vm.allTheRecords = response;
+      console.log(response.data);
+      vm.allTheRecords = response.data;
       console.log( vm.allTheRecords );
     // }), function myError( response ){
     //   console.log( response.statusText );
--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.listen( 8080, 'localhost', function( req, res ){
 // 27017 is default mongo port
 mongoose.connect( 'localhost:27017/meanie' );
 
-var ourSchema = new  mongoose.Schema({
+var ourSchema = mongoose.Schema({
   name: String,
   location: String
   });
@@ -30,20 +30,27 @@ app.get( '/getRecords', function( req, res ){
     console.log("in get records route on server");
 // get and send back all the things
     ourModel.find().then( function( data ){
-    res.send( data );
+      res.send( data );
   });
 });
 
 
 app.post( '/testPost', function( req, res ){
   console.log( 'req.body.name: ' + req.body.name );
+  console.log( 'req.body.location: ' + req.body.location );
+
 // retrieved the req.body
 // putting it into an object to be saved in the db
   var recordToAdd={
     name:req.body.name,
     location:req.body.location
   };
+
+  console.log( 'record to add: ' + recordToAdd );
+
 // create new record
-  var newRecord=ourModel( recordToAdd );
-  newRecord.save();
+  var newRecord = ourModel( recordToAdd );
+  newRecord.save().then(function(){
+    res.sendStatus(200);
+  });
   });
